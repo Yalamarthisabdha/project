89b8107048bd31c0006742a670195c6f2ebdef65
chandrika2005 <vamsiadapa95@gmail.com>
2023-11-15 15:30:07 +0530

    list

---
+++ list/analyticsAPI.js
@@ -0,0 +1,10 @@
+// analyticsRoutes.js
+const express = require('express');
+const router = express.Router();
+
+// Define your analytics list route
+router.get('/list', (req, res) => {
+  // Fetch and send analytics list data from MongoDB
+});
+
+module.exports = router;
